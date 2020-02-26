cd1a609774ad902377d0369ed538a082786ed737
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-26 05:08:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,52 @@
 # Make your shoe class here!
+
+class Shoe 
+  def initialize(brand)
+    @brand = brand
+  end 
+  
+  def brand
+    @brand
+  end
+  
+  def color=(color) #writer
+    @color = color
+  end 
+  
+  def color #reader 
+    @color
+  end
+  
+  def size=(size) #writer
+    @size = size
+  end
+  
+  def size #reader
+    @size
+  end 
+  
+  def material=(material)
+    @material = material
+  end
+  
+  def material
+    @material
+  end
+  
+  def condition=(condition)
+    @condition = condition
+  end
+  
+  def condition
+    @condition
+  end
+  
+  def cobble=(cobble)
+    puts "The shoe has been repaired"
+  end 
+  
+  def cobble
+    @cobble
+  end
+end
+
